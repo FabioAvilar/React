bc5cfe7b6efb63f332e64bb10a8a796a251cd55e
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-12 17:41:06 +0200

    course of react

---
+++ meu-projeto/src/components/Footer.js
@@ -0,0 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li><FaFacebook /></li>
+                <li><FaInstagram /></li>
+                <li><FaLinkedin /></li>
+                <p>Nosso rodapé</p>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
