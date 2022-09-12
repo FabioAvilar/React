e2c71ce3d055a8117a57ead416a958156b65de1c
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-12 17:40:28 +0200

    course of react

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,17 +1,30 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
 import './App.css';
-import OutraLista from './components/OutraLista';
+import Footer from './components/Footer';
+import Contato from './pages/Contato';
+import Empresa from './pages/Empresa';
+import Home from './pages/Home';
 
-function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
-  
+function App() {  
   return (
-    <div className="App">
-      <h1>Renderização de Listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={''}/>
-    </div>
-  );
+    // <Router>
+    //   <ul>
+    //     <li><Link to="/">Home</Link></li>
+    //     <li><Link to="/emprese">Empresa</Link></li>
+    //     <li><Link to="/contato">Contato</Link></li>
+    //   </ul>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} > </Route>
+    //     <Route path="/" element={<Contato />} > </Route>
+    //     <Route path="/" element={<Empresa />} > </Route>
+    //   </Routes>
+    // </Router>
+
+    <Footer />
+  )
+  
 }
+
 
 export default App;
