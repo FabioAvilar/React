e59f5d5297c4d8367362407dae778d34361c03f2
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-13 16:51:36 +0200

    iniciate a project

---
+++ costs/src/App.js
@@ -0,0 +1,36 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo projeto</Link>
+      </ul>
+      <Container>
+        <Routes>
+
+          <Route exact path='/' element={<Home />} />
+
+          <Route exact path='/company' element={<Company />} />
+
+          <Route exact path='/contact' element={<Contact />} />
+
+          <Route exact path='/newproject' element={<NewProject />} />
+
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
