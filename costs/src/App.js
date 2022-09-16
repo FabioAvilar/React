55c2bca89838bd6ceb3d18b238ff7ca206be2f27
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-16 12:33:10 +0200

    today i worked in footer and navbar

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,21 +3,22 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from './components/pages/Projects'
+
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo projeto</Link>
-      </ul>
-      <Container>
+      <Navbar />
+      <Container customClass='min-height'>
         <Routes>
 
           <Route exact path='/' element={<Home />} />
+
+          <Route exact path='/projects' element={<Projects />} />
 
           <Route exact path='/company' element={<Company />} />
 
@@ -28,7 +29,7 @@ function App() {
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
